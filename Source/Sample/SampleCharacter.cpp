c2a822510484c3e06ea9599b006d50a5bdb98705
Nauja <a@a.com>
2020-06-02 20:06:33 +0200

    Remove starter content

--- Source/Sample/SampleCharacter.cpp
+++ Source/Sample/SampleCharacter.cpp
@@ -27,9 +27,9 @@ ASampleCharacter::ASampleCharacter()
 	// Configure character movement
 	GetCharacterMovement()->GravityScale = 2.0f;
 	GetCharacterMovement()->AirControl = 0.80f;
-	GetCharacterMovement()->JumpZVelocity = 1000.f;
+	GetCharacterMovement()->JumpZVelocity = 800.f;
 	GetCharacterMovement()->GroundFriction = 3.0f;
-	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
+	GetCharacterMovement()->MaxWalkSpeed = 225.0f;
 	GetCharacterMovement()->MaxFlySpeed = 600.0f;
 
 	// Lock character motion onto the XZ plane, so the character can't move in or out of the screen
